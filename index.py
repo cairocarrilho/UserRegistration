2eb8e4846443e043fc57e60c8346fe13bd602ef6
Cairo Carrilho <cairojunior@gmail.com>
2023-06-03 16:26:58 -0300

    commit do cadastroUsuario

---
+++ index.py
@@ -0,0 +1,18 @@
+nome=[]
+senha=[]
+endereco=[]
+estado=[]
+cidade=[]
+cep=[]
+
+cadastroQtde = int(input("Quantos cadastro deseja realizar? "))
+
+for cadastro in range(cadastroQtde):
+    nome.append(input("Digite o nome do cliente: "))
+    senha.append(int(input("Digite a senha (somente senha numerica): ")))
+    endereco.append(input("digite seu endereco: "))
+    estado.append(input("Digite o nome do seu Estado: "))
+    cidade.append(input("Digite o nome da sua cidade: "))
+    cep.append(int(input("Digite seu CEP(sem traço e ponto): ")))
+    print(nome,senha,endereco,estado,cidade,cep)
+    print()
